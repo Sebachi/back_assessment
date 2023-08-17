9263930003172628dc5ede72ece634bb6295ba36
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-16 19:55:46 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(`${__dirname}/db.json`);
+const router = jsonServer.router(`./db.json`);
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    res.header('Access-Control-Allow-Methods', '*')
-    next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 server.use(router);
-server.use(middlewares)
+server.use(middlewares);
 
 server.listen(port, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
